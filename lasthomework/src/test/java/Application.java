32f3e8afdd56a3fea7b8194f1260ca5045b7a0ca
Bohdan <115922093+BohdanKulyk@users.noreply.github.com>
2022-11-05 19:19:31 +0200

    Add files via upload

---
+++ lasthomework/src/test/java/Application.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pages.*;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Application {
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @Test
+    public void canAddCar() {
+        HomePage homepage = new HomePage().open();
+        homepage.header.openLoginModal();
+        LoginModal loginModal = new LoginModal();
+        loginModal.login("work.kulykbogdan@gmail.com","Time13310");
+        GaragePage garagePage = new GaragePage();
+        garagePage.notifications.checkMessage("You have been successfully logged in");
+        garagePage.openAddCarPopup();
+        garagePage.enterDataCar();
+        garagePage.notifications.checkMessage("Car added");
+
+    }
+    @Test
+    public void canLogoutViaHeader() {
+        HomePage homepage = new HomePage().open();
+        homepage.header.openLoginModal();
+        LoginModal loginModal = new LoginModal();
+        loginModal.login("work.kulykbogdan@gmail.com","Time13310");
+        GaragePage garagePage = new GaragePage();
+        garagePage.notifications.checkMessage("You have been successfully logged in");
+        Header header = new Header();
+        header.logoutHeader();
+
+    }
+
+    @Test
+    public void canLogoutViaSideMenu(){
+        HomePage homepage = new HomePage().open();
+        homepage.header.openLoginModal();
+        LoginModal loginModal = new LoginModal();
+        loginModal.login("work.kulykbogdan@gmail.com","Time13310");
+        GaragePage garagePage = new GaragePage();
+        garagePage.notifications.checkMessage("You have been successfully logged in");
+        Sidemenu sidemenu = new Sidemenu();
+        sidemenu.logoutSideMenu();
+
+    }
+}
+
